a312e0393511cda429615492b4a82a67cbfccb4a
amichalek96942 <a.michalek.954@studms.ug.edu.pl>
2025-10-26 15:52:13 +0100

    Program kalkulator

---
+++ kalkulator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+int a = 10;
+int b = 5;
+
+printf("Wartosc dodawania a i b to: %d\n", a+b);
+printf("wartosc odejmowania a i b to: %d\n", a-b);
+printf("Wartosc mnozenia a i b to: %d\n", a*b);
+printf("Wartosc dzielenia a i b to: %d\n", a/b);
+
+return 0;
+}
